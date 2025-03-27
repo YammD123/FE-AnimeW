79be0156b35b4603ef0242883f397baddcba96e9
YammD123 <siamqueen52@gmail.com>
2025-03-28 02:37:41 +0700

    gird in details

--- src/routes/detail/[id]/+page.svelte
+++ src/routes/detail/[id]/+page.svelte
@@ -64,7 +64,7 @@
 		<span class="mr-2 h-6 w-1 bg-gradient-to-b from-purple-500 to-blue-500"></span>
 		Charachters
 	</h2>
-	<div class="grid grid-cols-5 gap-3 pt-6">
+	<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3 pt-6">
 		{#each data.characters.data as dataCharacters}
 			<div class="relative w-64">
 				<img
